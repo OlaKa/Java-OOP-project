8fac87a25fd3db8148822d581c0e308f8943d97a
katan <katan@172.20.201.133>
2016-11-15 23:19:59 +0100

    Added methods

--- Java-OOP-project/src/com/gmail/murmeldjur/oopproj/BinaryTree.java
+++ Java-OOP-project/src/com/gmail/murmeldjur/oopproj/BinaryTree.java
@@ -1,133 +1,234 @@
-package com.gmail.murmeldjur.oopproj;
+//package com.gmail.murmeldjur.binarytree;
 
-/*Binary tree for nodes */
+
+//import com.sun.org.apache.xpath.internal.SourceTree;
+
 public class BinaryTree {
 
-	Node root;
+    Node root;
 
-	public void addNode(int key, String name) {
+    public void addNode(int key, String name) {
 
-		Node newNode = new Node(key, name);
+        Node newNode = new Node(key, name);
 
-		if (root == null) {
+        if (root == null) {
 
-			root = newNode;
-		} else {
+            root = newNode;
+        } else {
 
-			Node focusNode = root;
+            Node focusNode = root;
 
-			Node parent;
+            Node parent;
 
-			while (true) {
-				parent = focusNode;
+            while (true) {
+                parent = focusNode;
 
-				if (key < focusNode.key) {
+                if (key < focusNode.key) {
 
-					focusNode = focusNode.leftChild;
-					if (focusNode == null) {
+                    focusNode = focusNode.leftChild;
+                    if (focusNode == null) {
 
-						parent.leftChild = newNode;
-						return;
-					}
-				} else {
-					focusNode = focusNode.rightChild;
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                } else {
+                    focusNode = focusNode.rightChild;
 
-					if (focusNode == null) {
-						parent.rightChild = newNode;
-						return;
-					}
-				}
-			}
-		}
-	}
+                    if (focusNode == null) {
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+    }
 
-	public void inOrderTraverseTree(Node focusNode) {
+    public void inOrderTraverseTree(Node focusNode) {
 
-		if (focusNode != null) {
-			inOrderTraverseTree(focusNode.leftChild);
-			System.out.println(focusNode);
-			inOrderTraverseTree(focusNode.rightChild);
-		}
-	}
+        if (focusNode != null) {
+            inOrderTraverseTree(focusNode.leftChild);
+            System.out.println(focusNode);
+            inOrderTraverseTree(focusNode.rightChild);
+        }
+    }
 
-	public void preOrderTraverseTree(Node focusNode) {
+    public void preOrderTraverseTree(Node focusNode) {
 
-		if (focusNode != null) {
-			System.out.println(focusNode);
-			preOrderTraverseTree(focusNode.leftChild);
-			preOrderTraverseTree(focusNode.rightChild);
-		}
-	}
+        if (focusNode != null) {
+            System.out.println(focusNode);
+            preOrderTraverseTree(focusNode.leftChild);
+            preOrderTraverseTree(focusNode.rightChild);
+        }
+    }
 
-	public void postOrderTraverseTree(Node focusNode) {
+    public void postOrderTraverseTree(Node focusNode) {
 
-		if (focusNode != null) {
+        if (focusNode != null) {
 
-			postOrderTraverseTree(focusNode.leftChild);
-			postOrderTraverseTree(focusNode.rightChild);
+            postOrderTraverseTree(focusNode.leftChild);
+            postOrderTraverseTree(focusNode.rightChild);
 
-			System.out.println(focusNode);
-		}
-	}
+            System.out.println(focusNode);
+        }
+    }
+    public Node findNode(int key) {
 
-	public Node findNode(int key) {
+        Node focusNode = root;
 
-		Node focusNode = root;
+        while (focusNode.key != key) {
 
-		while (focusNode.key != key) {
+            if (key < focusNode.key) {
 
-			if (key < focusNode.key) {
+                focusNode = focusNode.leftChild;
+            } else {
 
-				focusNode = focusNode.leftChild;
-			} else {
+                focusNode = focusNode.rightChild;
+            }
+            if (focusNode == null)
+                return null;
+        }
+        return focusNode;
+    }
 
-				focusNode = focusNode.rightChild;
-			}
-			if (focusNode == null)
-				return null;
-		}
-		return focusNode;
-	}
+    public boolean remove(int key) {
 
-	public static void main(String[] args) {
-		BinaryTree theTree = new BinaryTree();
+        Node focusNode = root;
+        Node parent = root;
 
-		theTree.addNode(50, "Boss");
-		theTree.addNode(25, "Vice pres.");
-		theTree.addNode(15, "Office Manager");
-		theTree.addNode(30, "Secretary");
-		theTree.addNode(75, "Sales Manager");
-		theTree.addNode(85, "Salesman 1");
+        boolean isItALeftChild = true;
 
-		theTree.inOrderTraverseTree(theTree.root);
-		System.out.println("");
-		System.out.println("--Pre Order Traversal--");
-		theTree.preOrderTraverseTree(theTree.root);
-		System.out.println("");
-		System.out.println("--Post Order Traversal--");
-		theTree.postOrderTraverseTree(theTree.root);
+        while (focusNode.key != key) {
+            parent = focusNode;
 
-		System.out.println("Search for 85");
+            if (key < focusNode.key) {
+                isItALeftChild = true;
+                focusNode = focusNode.leftChild;
+            } else {
+                isItALeftChild = false;
+                focusNode = focusNode.rightChild;
+            }
+            if (focusNode == null)
+                return false;
+        }
+        if (focusNode.leftChild == null && focusNode.rightChild == null) {
 
-		System.out.println(theTree.findNode(85));
+            if (focusNode == root)
+                root = null;
 
-	}
+            else if (isItALeftChild)
+                parent.leftChild = null;
+
+            else
+                parent.leftChild = null;
+        } else if (focusNode.rightChild == null) {
+            if (focusNode == root)
+                root = focusNode.leftChild;
+            else if (isItALeftChild)
+                parent.leftChild = focusNode.leftChild;
+            else
+                parent.rightChild = focusNode.leftChild;
+        } 
+                    // If no left child
+
+	        else if (focusNode.leftChild == null) {
+                if (focusNode == root)
+                    root = focusNode.rightChild;
+                    // If focus Node was on the left of parent
+                    // move the focus Nodes right child up to the
+                    // parent node
+                else if (isItALeftChild)
+                    parent.leftChild = focusNode.rightChild;
+                    // Vice versa for the left child
+                else
+                    parent.rightChild = focusNode.rightChild;
+            }
+            // Two children so I need to find the deleted nodes
+            // replacement
+
+            else {
+                Node replacement = getReplacementNode(focusNode);
+                // If the focusNode is root replace root
+                // with the replacement
+                if (focusNode == root)
+                    root = replacement;
+                    // If the deleted node was a left child
+                    // make the replacement the left child
+
+                else if (isItALeftChild)
+                    parent.leftChild = replacement;
+                    // Vice versa if it was a right child
+                else
+                    parent.rightChild = replacement;
+
+                replacement.leftChild = focusNode.leftChild;
+            }
+            return true;
+        }
+    
+
+    public Node getReplacementNode(Node replacedNode) {
+        Node replacementParent = replacedNode;
+        Node replacement = replacedNode;
+        Node focusNode = replacedNode.rightChild;
+
+        // While there are no more left children
+        while (focusNode != null) {
+            replacementParent = replacement;
+            replacement = focusNode;
+            focusNode = focusNode.leftChild;
+        }
+        // If the replacement isn't the right child
+        // move the replacement into the parents
+        // leftChild slot and move the replaced nodes
+        // right child into the replacements rightChild
+        if (replacement != replacedNode.rightChild) {
+            replacementParent.leftChild = replacement.rightChild;
+
+            replacement.rightChild = replacedNode.rightChild;
+        }
+        return replacement;
+    }
+
+
+    public static void main(String[] args) {
+        BinaryTree theTree = new BinaryTree();
+
+        theTree.addNode(25, "Boss");
+        theTree.addNode(50, "Vice pres.");
+        theTree.addNode(15, "Office Manager");
+        theTree.addNode(30, "Secretary");
+        theTree.addNode(75, "Sales Manager");
+        theTree.addNode(85, "Salesman 1");
+
+        theTree.inOrderTraverseTree(theTree.root);
+        System.out.println("");
+        System.out.println("--Pre Order Traversal--");
+        theTree.preOrderTraverseTree(theTree.root);
+        System.out.println("");
+        System.out.println("--Post Order Traversal--");
+        theTree.postOrderTraverseTree(theTree.root);
+
+        System.out.println("Search for 85");
+
+        System.out.println(theTree.findNode(85));
+
+    }
 }
 
 class Node {
-	int key;
-	String name;
+    int key;
+    String name;
 
-	Node leftChild;
-	Node rightChild;
+    Node leftChild;
+    Node rightChild;
 
-	Node(int key, String name) {
-		this.key = key;
-		this.name = name;
-	}
+    Node(int key, String name) {
+        this.key = key;
+        this.name = name;
+    }
 
-	@Override
-	public String toString() {
-		return name + " has key " + key;
-	}
+    @Override
+    public String toString() {
+        return name + " has key " + key;
+    }
 }
